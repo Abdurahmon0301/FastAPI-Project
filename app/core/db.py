ddb3484de0226058fead93530bc8890fb47bacb9
Abduramhon0301 <abduraxmonadbusattorov2012@gmail.com>
2026-02-24 16:40:39 +0500

    Refactor database URL configuration in db.py and add app.db file

--- app/core/db.py
+++ app/core/db.py
@@ -3,14 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLite (lokal fayl)
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 # PostgreSQL uchun:
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/postgres"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    # connect_args={"check_same_thread": False}  # Faqat SQLite uchun
+    connect_args={"check_same_thread": False}  # Faqat SQLite uchun
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
